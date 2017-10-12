9f55a4418b13e1ac146d082698aee3df6b7f4531
Jonathan Dursi <jonathan@dursi.ca>
2017-10-12 11:39:41 -0400

    Add BLAS compilation tests

--- tests/test_compilation.py
+++ tests/test_compilation.py
@@ -6,11 +6,15 @@ modules = ["CONS", "DNA", "FIB", "FIBD", "GC", "GRPH", "HAMM",
            "PPER", "PROB", "PROT", "PRTM", "REVC", "REVP", "RNA", "SIGN",
            "SPLC", "SSEQ", "SUBS", "TRAN"]
 
-need_blas = ["IEV", "LIA"]
+modules_need_blas = ["IEV", "LIA"]
 
 
 def compile_idfn(module):
     return "compile_" + module
+
+
+def compile_blas_idfn(module):
+    return "compile_" + module + "_blas"
 
 
 @pytest.mark.parametrize("module", modules, ids=compile_idfn)
@@ -20,5 +24,20 @@ def test_basic_compilation(module):
     executable = lowercase
 
     ret_val = subprocess.check_call(['chpl', '-o', executable, sourcecode],
+                                    cwd='./'+module)
+    assert ret_val == 0
+
+
+@pytest.mark.parametrize("module", modules_need_blas, ids=compile_blas_idfn)
+def test_basic_compilation_blas(module):
+    lowercase = module.lower()
+    sourcecode = lowercase+'.chpl'
+    executable = lowercase
+
+    BLASLIB = '/usr/local/opt/openblas/lib'
+    BLASINC = '/usr/local/opt/openblas/include'
+    ret_val = subprocess.check_call(['chpl', '-o', executable, sourcecode,
+                                     '-L'+BLASLIB, '-llapack', '-lblas',
+                                     '-I'+BLASINC],
                                     cwd='./'+module)
     assert ret_val == 0
