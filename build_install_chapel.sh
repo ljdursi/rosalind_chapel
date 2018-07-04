fa78a26551740148821afbd7ad633b80996fad54
Jonathan Dursi <jonathan@dursi.ca>
2018-07-03 20:36:27 -0400

    Switch to 1.17.1, no longer need the patch

--- build_install_chapel.sh
+++ build_install_chapel.sh
@@ -1,38 +1,14 @@
 #!/bin/bash
 cd || exit
-if [[ ! -f ~/chapel-1.16.0/util/setchplenv.bash ]]
+VERSION="1.17.1"
+if [[ ! -f ~/chapel-${VERSION}/util/setchplenv.bash ]]
 then
-    wget https://github.com/chapel-lang/chapel/releases/download/1.16.0/chapel-1.16.0-1.tar.gz
-    tar xzf chapel-1.16.0-1.tar.gz
-    rm chapel-1.16.0-1.tar.gz
-    cd chapel-1.16.0 || exit
+    wget https://github.com/chapel-lang/chapel/releases/download/${VERSION}/chapel-${VERSION}.tar.gz
+    tar xzf chapel-${VERSION}.tar.gz
+    rm chapel-${VERSION}.tar.gz
+    cd chapel-${VERSION} || exit
     source util/setchplenv.bash
     make
     export CHPL_AUX_FILESYS=curl
-    patch -p0 <<EOF
---- modules/packages/Curl.chpl  2017-10-10 17:45:03.000000000 -0400
-+++ modules/packages/Curl-new.chpl  2017-10-13 13:03:02.000000000 -0400
-@@ -299,18 +299,18 @@
-    :arg arg: the value to set the curl option specified by opt.
-    :type arg: `int`, `string`, `bool`, or `slist`
- */
--proc file.setopt(opt:c_int, arg):bool {
-+proc file.setopt(opt:c_int, arg):bool throws {
-   var err:syserr = ENOERR;
-
-   if (arg.type == slist) && (slist.home != this.home) {
--    ioerror(EFAULT:syserr, "in file.setopt(): slist, and curl handle do not reside on the same locale");
-+    try ioerror(EFAULT:syserr, "in file.setopt(): slist, and curl handle do not reside on the same locale");
-   }
-
-   on this.home {
-     err = chpl_curl_set_opt(this._file_internal, opt, arg);
-   }
-
--  if err then ioerror(err, "in file.setopt(opt:c_int, arg)");
-+  if err then try ioerror(err, "in file.setopt(opt:c_int, arg)");
-   return true;
- }
-EOF
     make
 fi
