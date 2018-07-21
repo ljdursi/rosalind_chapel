f86f9fbedad5bf487348b730de0979e4d64e29c9
Jonathan Dursi <jonathan@dursi.ca>
2018-07-21 15:12:17 -0400

    Tests for MULT

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -15,7 +15,7 @@ modules = ["CONS", "FIBD", "HAMM", "LIA", "PROT", "DNA", "GC",
            "AFRQ", "CUNR", "QRT", "GLOB", "PCOV", "PRSM", "LOCA",
            "SIMS", "CTEA", "SEXL", "CSTR", "SGRA", "ROOT", "SUFF",
            "WFMD", "OAP", "PDPL", "FOUN", "MEND", "GCON", "EBIN",
-           "SPTD", "OSYM", "GAFF", "LAFF", "GASM", "ASMQ"]
+           "SPTD", "OSYM", "GAFF", "LAFF", "GASM", "ASMQ", "MULT"]
 
 
 def run_idfn(module):
